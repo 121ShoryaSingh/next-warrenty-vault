c4bf0153d9300066d38f5e5717079675831316c5
121ShoryaSingh <shoryasingh810@gmail.com>
2025-10-15 03:20:31 +0530

    made changes in layout.tsx

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+
+export default function appLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
