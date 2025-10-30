e7b418eee39273bdfa2b55dff4e25f98d566edf8
121ShoryaSingh <shoryasingh810@gmail.com>
2025-10-31 01:45:50 +0530

    created dashboard

---
+++ app/(app)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Section from '@/components/Section';
+
+export default function Dashboard() {
+  return (
+    <div className="pt-32 bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950">
+      <div className="min-h-screen">
+        <Section>Dashboard</Section>
+      </div>
+    </div>
+  );
+}
