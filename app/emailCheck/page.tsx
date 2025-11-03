355ba1bd161fc933b243bfb1916d47bc70511b09
121ShoryaSingh <shoryasingh810@gmail.com>
2025-11-03 07:08:44 +0530

    added more thing email check

--- app/emailCheck/page.tsx
+++ app/emailCheck/page.tsx
@@ -11,19 +11,56 @@ import {
   FieldSet,
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import axios, { isAxiosError } from 'axios';
 import { ArrowLeft } from 'lucide-react';
+import { set } from 'mongoose';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function EmailCheck() {
   const router = useRouter();
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [isVerified, setIsVerified] = useState(true);
 
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post('/resetPassword', { email: email });
+
+      if (response.data.message === 'Password reset email sent') {
+        setIsVerified(true);
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          const errorData = error.response.data;
+        } else {
+          setError('Something went wrong, please try again');
+        }
+      }
+
+      //   else {
+      //         setError(error.response.data.message || 'Registation failed');
+      //       }
+      // }
+      //   } else if (error.request) {
+      //     setError('No server response. Check your connection.');
+      //   }
+      //   }
+      //   else {
+      //     setError(error.message);
+      //   }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleBackButton = () => {
     router.refresh();
@@ -44,7 +81,10 @@ export default function EmailCheck() {
           <div className=" mb-8">
             <Logo />
           </div>
-          <form className="max-w-xl py-10 bg-slate-950/50 mx-auto border border-slate-800 rounded-xl px-4">
+          <form
+            className="max-w-xl py-10 bg-slate-950/50 mx-auto border border-slate-800 rounded-xl px-4"
+            onSubmit={handleSubmit}
+          >
             <FieldGroup className="max-w-md mx-auto">
               <FieldSet>
                 <div className="text-slate-100 text-2xl font-medium text-center">
@@ -71,9 +111,22 @@ export default function EmailCheck() {
                   </Field>
                 </FieldGroup>
               </FieldSet>
-              <Button className="bg-blue-600 cursor-pointer hover:bg-blue-800 duration-300 ease-in">
+              <Button
+                className="bg-blue-600 cursor-pointer hover:bg-blue-800 duration-300 ease-in"
+                disabled={loading}
+                type="submit"
+              >
                 Submit
               </Button>
+              <div>{error && <p>{error}</p>}</div>
+              <div>
+                {isVerified && (
+                  <p>
+                    Email has been sent with instructions to reset their
+                    password.
+                  </p>
+                )}
+              </div>
             </FieldGroup>
           </form>
         </Section>
