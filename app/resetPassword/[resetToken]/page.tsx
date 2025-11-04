6ce1bb6430211dae0c81cc8afbc3a30d768b0637
Shorya <shoryasingh810@gmail.com>
2025-11-04 06:10:55 +0530

    created a dynamic route to update password

--- app/resetPassword/[resetToken]/page.tsx
+++ app/resetPassword/[resetToken]/page.tsx
@@ -1,1 +1,15 @@
-export default function resetPassword() {}
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useState } from 'react';
+
+export default function ResetPassword() {
+  const params = useParams();
+  const resetToken = params.resetToken as string;
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  return <div></div>;
+}
