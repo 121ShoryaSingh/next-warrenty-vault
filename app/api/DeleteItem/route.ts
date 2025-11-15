5ebaa5d4d8883b2f1571535771735c582839e450
121ShoryaSingh <shoryasingh810@gmail.com>
2025-11-15 13:02:27 +0530

    make delete route functional

--- app/api/DeleteItem/route.ts
+++ app/api/DeleteItem/route.ts
@@ -1,8 +1,47 @@
+import { Db } from '@/lib/Db';
+import Item from '@/model/Item';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function Delete(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const id = params.id;
+  try {
+    const id = params.id;
+
+    if (!id) {
+      return NextResponse.json(
+        {
+          message: 'Id is missing.',
+        },
+        { status: 400 }
+      );
+    }
+
+    await Db();
+
+    const item = Item.findByIdAndDelete({ id });
+
+    if (!item) {
+      return NextResponse.json(
+        {
+          message: 'Wrong id item not found',
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: 'Item deleted successfully',
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Failed to delete item', error);
+    return NextResponse.json(
+      { error: 'Internal server error failed to delete item' },
+      { status: 500 }
+    );
+  }
 }
