486aafa95a51cf6a848753fc0635922915e0a0d8
121ShoryaSingh <shoryasingh810@gmail.com>
2025-11-15 16:06:04 +0530

    change few thing in my download from r2 function

--- lib/DownloadFromR2.ts
+++ lib/DownloadFromR2.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-export async function DownloadToR2(key: string) {
+export async function DownloadFromR2(
+  key: string
+): Promise<
+  { success: true; url: string } | { success: false; error: unknown }
+> {
   try {
     const response = await axios.post('/api/PreSignedUrl', {
       key,
       operation: 'download',
     });
-
-    const { signedUrl } = await response.data;
-
-    window.open(signedUrl, '_blank');
+    const { signedUrl } = response.data;
+    return { success: true, url: signedUrl };
   } catch (error) {
     return { success: false, error };
   }
