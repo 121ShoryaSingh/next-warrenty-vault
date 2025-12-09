15ec70862c20f65162890e7cb5ba503415062cd3
121ShoryaSingh <shoryasingh810@gmail.com>
2025-12-09 12:57:01 +0530

    changing middleware to proxy

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-export { auth as middleware } from '@/app/api/(auth)/[...nextauth]/auth';
-
-export const config = {
-  matcher: ['/((?!api/ResetPassword).*)', '/((?!api/UpdateResetPassword).*)'],
-};
